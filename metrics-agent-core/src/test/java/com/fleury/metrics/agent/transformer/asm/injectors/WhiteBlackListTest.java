8b48a562d70557ba535758822cb3732c913d829d
Will Fleury <willfleury@gmail.com>
2017-09-26 10:32:27 +0200

    white & black list

--- metrics-agent-core/src/test/java/com/fleury/metrics/agent/transformer/asm/injectors/WhiteBlackListTest.java
+++ metrics-agent-core/src/test/java/com/fleury/metrics/agent/transformer/asm/injectors/WhiteBlackListTest.java
@@ -1,5 +1,6 @@
 package com.fleury.metrics.agent.transformer.asm.injectors;
 
+import static com.fleury.metrics.agent.config.Configuration.convertConfigClassNameToInternal;
 import static java.util.Arrays.asList;
 import static org.junit.Assert.assertEquals;
 
@@ -13,7 +14,7 @@ public class WhiteBlackListTest extends BaseMetricTest {
     @Test
     public void allowWhiteListed() throws Exception {
         Configuration config = new Configuration();
-        List<String> whiteList = asList(CountedMethodClass.class.getName());
+        List<String> whiteList = asList(convertConfigClassNameToInternal(CountedMethodClass.class.getName()));
         config.setWhiteList(whiteList);
 
         Class<CountedMethodClass> clazz = execute(CountedMethodClass.class, config);
@@ -28,7 +29,7 @@ public class WhiteBlackListTest extends BaseMetricTest {
     @Test
     public void ignoreBlackListed() throws Exception {
         Configuration config = new Configuration();
-        List<String> blackList = asList(CountedMethodClass.class.getName());
+        List<String> blackList = asList(convertConfigClassNameToInternal(CountedMethodClass.class.getName()));
         config.setBlackList(blackList);
 
         Class<CountedMethodClass> clazz = execute(CountedMethodClass.class, config);
